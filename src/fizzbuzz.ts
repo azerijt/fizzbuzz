1dc580e953d999ef7edeb8c46bb80d457010fe8d
azerijt <azjuliantemir@gmail.com>
2023-01-31 10:43:39 +0000

    code passes test

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,4 +1,20 @@
-export default function fizzbuzz(n:number): any[] {
-  return ['a']
+export default function fizzbuzz(n:number): string {
+  let result = [];
+  for (let i =1; i<=n; i++){
+    if (i%3 ===0 && i%5 === 0){
+      result.push("FizzBuzz")
+    }
+    else if (i%3===0){
+      result.push("Fizz")
+
+    }
+    else if (i%5===0){
+      result.push("Buzz")
+    }
+    else {
+      result.push(i.toString())
+    }
+  }
+  return result.join(', ')
 }
 
