18663b668205ae8062ca34d93769bd7fc5ed4c09
azerijt <azjuliantemir@gmail.com>
2023-01-31 10:20:26 +0000

    test

---
+++ src/fizzbuzz.test.ts
@@ -0,0 +1,9 @@
+import fizzbuzz from './fizzbuzz';
+
+describe("fizzbuzz", () => {
+  it("returns the correct output for n = 15", () => {
+    expect(fizzbuzz(15)).toEqual(
+      "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz"
+    );
+  });
+});
---
+++ src/fizzbuzz.ts
@@ -0,0 +1,4 @@
+export default function fizzbuzz(n:number): any[] {
+  return ['a']
+}
+
