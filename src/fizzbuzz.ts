72b942219a5610a3d14e554b61c1f82178c7901c
azerijt <azjuliantemir@gmail.com>
2023-01-31 11:43:05 +0000

    switch case

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,16 +1,21 @@
 export default function fizzBuzz(n: number): string {
   let result = [];
   for (let i = 1; i <= Math.abs(n); i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-      result.push("FizzBuzz");
-    } else if (i % 3 === 0) {
-      result.push("Fizz");
-    } else if (i % 5 === 0) {
-      result.push("Buzz");
-    } else {
-      result.push((n > 0 ? i : -i).toString());
-
+    let value: string;
+    switch (true) {
+      case i % 3 === 0 && i % 5 === 0:
+        value = "FizzBuzz";
+        break;
+      case i % 3 === 0:
+        value = "Fizz";
+        break;
+      case i % 5 === 0:
+        value = "Buzz";
+        break;
+      default:
+        value = (n > 0 ? i : -i).toString();
     }
+    result.push(value);
   }
 
   return result.join(", ");
